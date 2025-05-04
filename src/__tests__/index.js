4ec6560f9dd8029c731903bb81b4eff8a52b6eba
hritik5102 <hritik.dj2000@gmail.com>
2025-05-04 13:37:05 +0530

    Add subtract operation test

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,6 +1,6 @@
-import { add } from "../../index";
+import { add, subtract } from "../../index";
 
-describe("Math operation", () => {
+describe("Add operation", () => {
   it("should add numbers 1 and 2 correctly", () => {
     expect(add(1, 2)).toEqual(3);
   });
@@ -15,5 +15,23 @@ describe("Math operation", () => {
 
   it("should add numbers 1 and -2 correctly", () => {
     expect(add(1, -2)).toEqual(-1);
+  });
+});
+
+describe("Subtract operation", () => {
+  it("should subtract numbers 1 and 2 correctly", () => {
+    expect(subtract(1, 2)).toEqual(-1);
+  });
+
+  it("should subtract numbers -1 and -2 correctly", () => {
+    expect(subtract(-1, -2)).toEqual(1);
+  });
+
+  it("should subtract numbers -1 and 2 correctly", () => {
+    expect(subtract(-1, 2)).toEqual(-3);
+  });
+
+  it("should subtract numbers 1 and -2 correctly", () => {
+    expect(subtract(1, -2)).toEqual(3);
   });
 });
