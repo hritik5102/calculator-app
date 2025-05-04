bdb57d8fe782c126f3f7ff7d725c83136f666e43
hritik5102 <hritik.dj2000@gmail.com>
2025-05-04 13:40:59 +0530

    Add multiply operation test

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,4 +1,4 @@
-import { add, subtract } from "../../index";
+import { add, subtract, multiply } from "../../index";
 
 describe("Add operation", () => {
   it("should add numbers 1 and 2 correctly", () => {
@@ -33,5 +33,27 @@ describe("Subtract operation", () => {
 
   it("should subtract numbers 1 and -2 correctly", () => {
     expect(subtract(1, -2)).toEqual(3);
+  });
+});
+
+describe("Multiply operation", () => {
+  it("should multiply numbers 1 and 2 correctly", () => {
+    expect(multiply(1, 2)).toEqual(2);
+  });
+
+  it("should multiply numbers -2 and -2 correctly", () => {
+    expect(multiply(-2, -2)).toEqual(4);
+  });
+
+  it("should multiply numbers -2 and 5 correctly", () => {
+    expect(multiply(-2, 5)).toEqual(-10);
+  });
+
+  it("should multiply numbers 0.5 and 5 correctly", () => {
+    expect(multiply(0.5, 5)).toEqual(2.5);
+  });
+
+  it("should multiply numbers 0.5 and 0.5 correctly", () => {
+    expect(multiply(0.5, 0.5)).toEqual(0.25);
   });
 });
